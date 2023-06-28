dcaca5b3b7a847ef53918bb660824039b6fae057
LucasCHBR <124623557+LucasCHBR@users.noreply.github.com>
2023-06-28 18:03:19 +0100

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAd4CSnEcQZWp6RJJrIWMnEaYFpqf-q0ac",
+    authDomain: "project93-9dcd5.firebaseapp.com",
+    projectId: "project93-9dcd5",
+    storageBucket: "project93-9dcd5.appspot.com",
+    messagingSenderId: "202934541232",
+    appId: "1:202934541232:web:60fa55e675f60c03e9d475"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
